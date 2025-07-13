f87a74c9b154c84079f7085224bf1ff33465e0d6
Thiago Natanael <thiagonatanael@gmail.com>
2025-07-13 09:38:27 +0000

    Adding routes to swagger

--- app/routes.php
+++ app/routes.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-use App\HelloController;
+use App\Controllers\HelloController;
 use Slim\App;
 
 return function (App $app) {
@@ -11,4 +11,26 @@ return function (App $app) {
 
     // protected routes
     $app->get('/bye/{name}', HelloController::class . ':bye');
+    
+    // OpenAPI documentation
+    $app->get('/api/documentation', function ($request, $response) {
+
+        $openapi = (new \OpenApi\Generator())->generate([
+            __DIR__ . '/../src/Controllers',
+            __DIR__ . '/../src/OpenApi'
+        ]);
+        
+        // Add JSON_PRETTY_PRINT and ensure proper encoding
+        $response->getBody()->write($openapi->toJson(JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+        
+        return $response
+            ->withHeader('Content-Type', 'application/json')
+            ->withHeader('Access-Control-Allow-Origin', '*');
+    });
+    
+    // Add this after your other routes
+    $app->get('/swagger', function ($request, $response) {
+        include __DIR__ . '/../public/swagger-ui.php';
+        return $response;
+    });
 };
